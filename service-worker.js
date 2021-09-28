e1a38614e8ef2e73bbffba27d792691f08c5696f
Dimitris Vainanidis <dvainanidis@gmail.com>
2021-09-28 22:57:08 +0300

    0.1

---
+++ service-worker.js
@@ -0,0 +1,43 @@
+'use strict';
+
+self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+
+// This will work!
+
+workbox.routing.registerRoute(
+    new RegExp('/index.html$'),
+    new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+    new RegExp('.+\\.js$'),
+    new workbox.strategies.NetworkFirst()
+  );
+
+workbox.routing.registerRoute(
+    new RegExp('.+\\.css$'),
+    new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerRoute(
+    new RegExp('.+\\.png$'),
+    new workbox.strategies.NetworkFirst()
+);
+
+/*
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sw-cache').then(function(cache) {
+      return cache.add('index.html');
+    })
+  );
+});
+ 
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+*/
