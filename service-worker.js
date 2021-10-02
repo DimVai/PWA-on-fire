fc00abc5d7437615338b9a13a55899105a988085
Dimitris Vainanidis <dvainanidis@gmail.com>
2021-10-02 19:58:23 +0300

    1.0

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,18 @@
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
+// workbox.routing.registerRoute(
+//   new RegExp('.*'),      //all files
+//   new workbox.strategies.StaleWhileRevalidate()
+// );
+
+//prefer cache on external (starting with http/https) resources
 workbox.routing.registerRoute(
-  new RegExp('.*'),
-  new workbox.strategies.StaleWhileRevalidate()
+  new RegExp('^(http).*'),    
+  new workbox.strategies.CacheFirst()
+);
+
+//prefer internet on internal resources
+workbox.routing.registerRoute(
+  new RegExp('^(?!http).*'),
+  new workbox.strategies.NetworkFirst()
 );
