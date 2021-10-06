31977b9653320647132c2aa7e8c3a2d313ef494f
Dimitris Vainanidis <dvainanidis@gmail.com>
2021-10-06 19:55:07 +0300

    1.1

--- service-worker.js
+++ service-worker.js
@@ -4,14 +4,17 @@ self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/wo
 let regExpContains = (array) => new RegExp('.*('+array.join('|')+').*');
 let regExpContainsNot = (array) => new RegExp('^(?!.*('+array.join('|')+')).*');
 
+/** An array of strings that when appear on resourses URL, it is serverd from cache  */
+let cacheFirstArray = ["cdn","npm","googleapis","unpkg","jsdelivr"];
+
 //prefer cache on external (containing words like cdn, npm or googleapis) resources
 workbox.routing.registerRoute(
-  regExpContains(["cdn","npm","googleapis"]),    
+  regExpContains(cacheFirstArray),    
   new workbox.strategies.CacheFirst()
 );
 
 //prefer internet on internal resources
 workbox.routing.registerRoute(
-  regExpContainsNot(["cdn","npm","googleapis"]),   
-  new workbox.strategies.NetworkFirst()
-);
+  regExpContainsNot(cacheFirstArray),   
+  new workbox.strategies.NetworkFirst()     //Alternatively: strategies.StaleWhileRevalidate()
+); 
