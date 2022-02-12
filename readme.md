5586d7d645eadb753153ae337265ca799bc1afe9
Dimitris Vainanidis <dvainanidis@gmail.com>
2022-02-12 02:18:14 +0200

    1.2

--- readme.md
+++ readme.md
@@ -1,29 +1,24 @@
-PWA with code as minimal as possible, as regards manifest + service worker, but with jQuery + Bootstrap + custom CSS.
+PWA with code as minimal as possible, as regards manifest & service worker, but with jQuery & Bootstrap & custom CSS.
+You can just follow the instructions, copy the files, not change anything and you will have a PWA! 
 
 https://dimvai.github.io/PWA/
 
-What to do specifically:
+What to do specifically, in order:
 
-1. Make your app's icon, `logo.png` (along with your `favicon.ico`)
-2. Add the `manifest.json` (change it later if you like)
-3. Add the `service-worker.js` (change it later if you like)
-4. In your `index.html` add:
-
-
-In the `head` section:
+1. Set your app's icon file, `logo.png` (along with your `favicon.ico`)
+2. Add the `manifest.json` (change the properties appropriately)
+3. Add the `pwa.js` and the `service-worker.js` files (optionally change them, but you don't have to)
+4. In your `index.html` add the references to the above things. In the `head` section set:
 ```HTML
-<link rel="manifest" href="manifest.json">      
 <link rel="icon" href="favicon.ico" type="image/x-icon"/>
+<link rel="manifest" href="manifest.json">      
+
+<script src="pwa.js"></script>
 ```
 
-Anywhere else in HTML, preferably towards the end of the file (in the scripts section - before </body>):
-```HTML
-<script>
-    window.addEventListener('load', function(event) {
-        if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('service-worker.js')
-            .catch((err)=>console.debug('service worker not registered',err));
-        }
-    });
-</script>
-```
+Note that `pwa.js` calls the `service-worker.js`, so you do not call `service-worker.js` in your code. Also, by default, I have set:
+* the external resources to "Stale while revalidate" (it can be dangerous for external resources to "cache first"...)
+* the never-changed resources (like bootstrap/jquery files) to "Cache first" and 
+* the internal sources to "Network first". 
+
+It is sufficient to import `pwa.js` only on your main entry point, and not call it again on any other (sub)pages if you're sure that all users will pass through that entry point at least once. That being said, there's no harm in importing `pwa.js` in multiple filers.
--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,48 @@
-/* jshint ignore:start */
+'use strict';
+
+// import Workbox
 self.importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
 
+// disable console logs
+workbox.setConfig({ debug: false });   
+
+// You must delete the following line if you are using lazy-loading of versioned, pre-cached assets
+// or have other problems when updating the service worker. 
+// Else, it is recommended to do skipWaiting.
+// (skipWaiting: activate the new version of service worker now, instead of waiting for the next session to do so)
+self.addEventListener('install', event => { self.skipWaiting() });
+
+// when the new updated service worker (this file) gets activated
+self.addEventListener('activate', event => { 
+    // event.waitUntil( /* caching and other things to do before it is being installed */ );
+    console.debug('service worker activated', event);
+});
+
+
+// just helper functions
 let regExpContains = (array) => new RegExp('.*('+array.join('|')+').*');
 let regExpContainsNot = (array) => new RegExp('^(?!.*('+array.join('|')+')).*');
 
-/** An array of strings that when appear on resourses URL, it is serverd from cache  */
-let cacheFirstArray = ["cdn","npm","googleapis","unpkg","jsdelivr"];
+/** An array of strings that belong to local filenames that will never change */
+let unchangedResources = ['bootstrap','jquery'];
+/** An array of strings that indicate external resources */
+let externalResources = ["cdn","npm","googleapis","unpkg","jsdelivr"];
 
-//prefer cache on external (containing words like cdn, npm or googleapis) resources
+// 1. prefer cache on unchanged resources
 workbox.routing.registerRoute(
-  regExpContains(cacheFirstArray),    
-  new workbox.strategies.CacheFirst()
+    regExpContains(unchangedResources),    
+    new workbox.strategies.CacheFirst()
 );
 
-//prefer internet on internal resources
+// 2. prefer cache and updating on external resources
+// keep in mind that you should never use CacheFirst/CacheOnly on external resources! 
 workbox.routing.registerRoute(
-  regExpContainsNot(cacheFirstArray),   
-  new workbox.strategies.NetworkFirst()     //Alternatively: strategies.StaleWhileRevalidate()
+    regExpContains(externalResources),    
+    new workbox.strategies.StaleWhileRevalidate()       // Alternatively: NetworkFirst()
+);
+
+// 3. prefer internet on other resources that are updated frequently 
+workbox.routing.registerRoute(
+    regExpContainsNot([...unchangedResources,...externalResources]),   
+    new workbox.strategies.NetworkFirst()     // Alternatively: StaleWhileRevalidate()
 ); 
